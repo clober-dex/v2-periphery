a6accbe1ab144a712b4390cf7ae10a9621656bc9
Sean <detectivekim1@gmail.com>
2024-04-26 16:46:01 +0900

    refactor: add verify

--- deploy/Controller.ts
+++ deploy/Controller.ts
@@ -1,18 +1,9 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
-import { getDeployedAddress } from '../utils'
+import { deployWithVerify, getDeployedAddress } from '../utils'
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
-  const { deploy } = deployments
-
-  const { deployer } = await getNamedAccounts()
-
-  await deploy('Controller', {
-    from: deployer,
-    args: [await getDeployedAddress('BookManager')],
-    log: true,
-  })
+  await deployWithVerify(hre, 'Controller', [await getDeployedAddress('BookManager')])
 }
 
 deployFunction.tags = ['Controller']
