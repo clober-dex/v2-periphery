832206476316baf94ff656f876bc1c779da0884f
JChoy <jchoyeclipse@gmail.com>
2024-04-29 17:55:41 +0900

    feat(deploy): deploy on base

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,7 @@
-import { arbitrumSepolia } from 'viem/chains'
+import { arbitrumSepolia, base, berachainTestnet } from 'viem/chains'
 import { Address } from 'viem'
 
 export const BOOK_MANAGER: { [chainId: number]: Address } = {
   [arbitrumSepolia.id]: '0x3a90fbD5DbE4C82018A4Ac28406A50917dB91def',
+  [base.id]: '0x59fAD5b95e755034572702991ABBA937Cc90254a',
 }
