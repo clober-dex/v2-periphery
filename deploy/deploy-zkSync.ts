112a6a18f7911b1a5893c78fa25cc2b5d5ff9299
Sean <detectivekim1@gmail.com>
2024-05-07 11:24:43 +0900

    feat: add verify script

--- deploy/deploy-zkSync.ts
+++ deploy/deploy-zkSync.ts
@@ -30,17 +30,6 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   const bookManager = BOOK_MANAGER[chain.id]
   let deploymentFee = await deployer.estimateDeployFee(controllerArtifact, [bookManager])
 
-  // ⚠️ OPTIONAL: You can skip this block if your account already has funds in L2
-  // const depositHandle = await deployer.zkWallet.deposit({
-  //   to: deployer.zkWallet.address,
-  //   token: utils.ETH_ADDRESS,
-  //   amount: deploymentFee.mul(2),
-  // });
-  // // Wait until the deposit is processed on zkSync
-  // await depositHandle.wait();
-
-  // Deploy this contract. The returned object will be of a `Contract` type, similar to ones in `ethers`.
-  // `greeting` is an argument for contract constructor.
   let parsedFee = ethers.formatEther(deploymentFee)
   console.log(`The deployment is estimated to cost ${parsedFee} ETH`)
 
@@ -52,6 +41,12 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   // Show the contract info.
   const controllerAddress = await controllerContract.getAddress()
   console.log(`${controllerArtifact.contractName} was deployed to ${controllerAddress}`)
+
+  await hre.run("verify:verify", {
+    address: controllerAddress,
+    constructorArguments: [bookManager],
+    contract: 'src/Controller.sol:Controller'
+  })
 
   const viewerArtifact = await deployer.loadArtifact('BookViewer')
   deploymentFee = await deployer.estimateDeployFee(viewerArtifact, [bookManager])
@@ -66,4 +61,10 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   // Show the contract info.
   const viewerAddress = await viewerContract.getAddress()
   console.log(`${viewerArtifact.contractName} was deployed to ${viewerAddress}`)
+
+  await hre.run("verify:verify", {
+    address: viewerAddress,
+    constructorArguments: [bookManager],
+    contract: 'src/BookViewer.sol:BookViewer'
+  })
 }
