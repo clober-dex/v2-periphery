77053188e79c2206c77c55cc5c611d6a95b94397
Sean <detectivekim1@gmail.com>
2024-05-08 16:43:03 +0900

    feat: deploy `Controller`, `BookViewer`

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,5 +4,5 @@ import { Address } from 'viem'
 export const BOOK_MANAGER: { [chainId: number]: Address } = {
   [arbitrumSepolia.id]: '0x3a90fbD5DbE4C82018A4Ac28406A50917dB91def',
   [base.id]: '0x59fAD5b95e755034572702991ABBA937Cc90254a',
-  [berachainTestnet.id]: '0x982c57388101D012846aDC4997E9b073F3bC16BD',
+  [berachainTestnet.id]: '0x10C08D40918F93fEb7D48749d72842aDE0aF3c20',
 }
