e24b8c60ec3f3f43fedea27057f796f5b3bed835
JChoy <jchoyeclipse@gmail.com>
2024-05-09 12:38:44 +0900

    feat(deploy): re-deploy on bera testnet

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,5 +4,5 @@ import { Address } from 'viem'
 export const BOOK_MANAGER: { [chainId: number]: Address } = {
   [arbitrumSepolia.id]: '0x3a90fbD5DbE4C82018A4Ac28406A50917dB91def',
   [base.id]: '0x59fAD5b95e755034572702991ABBA937Cc90254a',
-  [berachainTestnet.id]: '0x10C08D40918F93fEb7D48749d72842aDE0aF3c20',
+  [berachainTestnet.id]: '0xA7e0051561D5b955F1014939FB54F71C7F4AEdF1',
 }
