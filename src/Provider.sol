f61b2a94fad9cf48da213aa30008753399117bbd
Sean <detectivekim1@gmail.com>
2024-05-13 17:59:38 +0900

    fix: `collect` before claim

--- src/Provider.sol
+++ src/Provider.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {IProvider} from "./interfaces/IProvider.sol";
+import {IBookManager} from "v2-core/interfaces/IBookManager.sol";
 import {Currency, CurrencyLibrary} from "v2-core/libraries/Currency.sol";
+import {IProvider} from "./interfaces/IProvider.sol";
 import {IProviderFactory} from "./interfaces/IProviderFactory.sol";
 
 contract Provider is IProvider {
@@ -10,12 +11,14 @@ contract Provider is IProvider {
 
     uint256 public constant RATE_PRECISION = 10 ** 6;
 
+    IBookManager private immutable _bookManager;
     IProviderFactory public immutable factory;
     address public immutable broker;
     uint256 public immutable shareRatio;
 
     constructor(address broker_, uint256 shareRatio_) {
         factory = IProviderFactory(msg.sender);
+        _bookManager = factory.bookManager();
         broker = broker_;
         shareRatio = shareRatio_;
     }
@@ -24,6 +27,7 @@ contract Provider is IProvider {
         address protocolTreasury = factory.treasury();
         for (uint256 i = 0; i < currencies.length; i++) {
             Currency currency = currencies[i];
+            _bookManager.collect(address(this), currency);
             uint256 balance = currency.balanceOfSelf();
             uint256 brokerShare = balance * shareRatio / RATE_PRECISION;
             uint256 protocolShare = balance - brokerShare;
