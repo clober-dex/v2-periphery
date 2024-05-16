ea34079946e1e4256472a0b5a4def5483d03f5be
JChoy <jchoyeclipse@gmail.com>
2024-05-16 18:47:49 +0900

    feat: use create3 when deploying

--- deploy/BookViewer.ts
+++ deploy/BookViewer.ts
@@ -1,13 +1,35 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
-import { deployWithVerify, BOOK_MANAGER } from '../utils'
+import { deployWithVerify, BOOK_MANAGER, deployCreate3WithVerify } from '../utils'
 import { getChain } from '@nomicfoundation/hardhat-viem/internal/chains'
+import { Address, encodeFunctionData } from 'viem'
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre
+  if (await deployments.getOrNull('BookViewer')) {
+    return
+  }
+
+  const deployer = (await getNamedAccounts())['deployer'] as Address
   const chain = await getChain(hre.network.provider)
-  await deployWithVerify(hre, 'BookViewer', [BOOK_MANAGER[chain.id]], null, true)
+  const implementation = (await deployWithVerify(hre, 'BookViewer_Implementation', [BOOK_MANAGER[chain.id]], {
+    contract: 'BookViewer',
+  })) as Address
+  const bookViewerArtifact = await hre.artifacts.readArtifact('BookViewer')
+  const initData = encodeFunctionData({
+    abi: bookViewerArtifact.abi,
+    functionName: '__BookViewer_init',
+    args: [deployer],
+  })
+  const viewer = await deployCreate3WithVerify(deployer, 0xffffn, 'BookViewer_Proxy', [implementation, initData], {
+    contract: 'ERC1967Proxy',
+  })
+  await deployments.save('BookViewer', {
+    address: viewer,
+    abi: bookViewerArtifact.abi,
+    implementation: implementation,
+  })
 }
 
 deployFunction.tags = ['BookViewer']
-deployFunction.dependencies = ['BookManager']
 export default deployFunction
