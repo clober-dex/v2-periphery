5d224e74a04fbd8a6ea724091cb9a1e92316a9d9
JChoy <jchoyeclipse@gmail.com>
2024-05-20 16:17:21 +0900

    feat(deploy): re-deploy on zkSyncSepoliaTestnet and zkSync

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,6 +5,6 @@ export const BOOK_MANAGER: { [chainId: number]: Address } = {
   [arbitrumSepolia.id]: '0xC528b9ED5d56d1D0d3C18A2342954CE1069138a4',
   [base.id]: '0x382CCccbD3b142D7DA063bF68cd0c89634767F76',
   [berachainTestnet.id]: '0x982c57388101D012846aDC4997E9b073F3bC16BD',
-  [zkSync.id]: '0x5961268BFd6b057c3ffA4709eDb920bD97011B13',
-  [zkSyncSepoliaTestnet.id]: '0x419DD5a4e74f96e57C1F8B8B46ae6855395A9de7',
+  [zkSync.id]: '0xAaA0e933e1EcC812fc075A81c116Aa0a82A5bbb8',
+  [zkSyncSepoliaTestnet.id]: '0xe59109Be1c74331E34BF938783a8a92Fd8499383',
 }
