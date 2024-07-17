6c82fa6060eab43f5a5af43f3a554f52fa0797aa
Sean <detectivekim1@gmail.com>
2024-07-17 20:51:06 +0900

    feat: `Arbitrage`

---
+++ src/interfaces/IArbitrage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {BookId} from "v2-core/libraries/BookId.sol";
+
+interface IArbitrage {
+    error NotOperator();
+
+    event SetOperator(address indexed operator, bool status);
+
+    function setOperator(address operator, bool status) external;
+
+    function arbitrage(BookId id, address router, bytes calldata data) external;
+}
